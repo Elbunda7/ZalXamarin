ea849f0fd10c1d2da79dd9c0f7f3547fa72fce1c
Michal Břemek <michal.bremek@seznam.cz>
2018-03-22 15:15:20 +0100

    Rest Api Client

--- ZalXamarin/ZalXamarin/Pages/Actions/ActionCreator.xaml.cs
+++ ZalXamarin/ZalXamarin/Pages/Actions/ActionCreator.xaml.cs
@@ -21,7 +21,7 @@ namespace ZalXamarin.Pages.Actions
             string type = TypeEntry.Text == null ? "" : TypeEntry.Text;
             string name = NameEntry.Text == null ? "" : NameEntry.Text;
             int days = DaysEntry.Text == null ? 0 : int.Parse(DaysEntry.Text);
-            Zal.Actions.InsertNewAction(name, type, datePicker.Date, days, 0, true);
+            Zal.Actions.InsertNewAction(name, type, datePicker.Date, datePicker.Date, 0, true);
             await Navigation.PopAsync();
         }
     }
